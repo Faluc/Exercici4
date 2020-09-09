e3f8ba65515516b1b1e4c4b35363e81eca850715
Faluc <61325596+Faluc@users.noreply.github.com>
2020-09-09 12:38:20 +0200

    Fase1 (JAVA)

---
+++ Fase1.java
@@ -0,0 +1,18 @@
+package exercici4;
+
+public class Fase1 {
+
+	public static void main(String[] args) {
+		int bitllet_5 = 5;
+		int bitllet_10= 10;
+		int bitllet_20 = 20;
+		int bitllet_50 = 50;
+		int bitllet_100 = 100;
+		int bitllet_200 = 200;
+		int bitllet_500 = 500;
+		int preu_total = 0;
+		String[] menu = new String[4];
+		int[] preu_plat = new int[4];
+	}
+
+}
